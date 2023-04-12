62e3907eaf3f221eceec74d131aa935c0fd7ffda
densuke5G <129590446+densuke5G@users.noreply.github.com>
2023-04-12 15:07:30 +0000

    lesson13

--- app/controllers/postimages_controller.rb
+++ app/controllers/postimages_controller.rb
@@ -2,7 +2,7 @@ class PostimagesController < ApplicationController
   def new
     @postimage = Postimage.new
   end
-  
+
   def create
     @postimage = Postimage.new(postimage_params)
     @postimage.user_id = current_user.id
@@ -11,15 +11,23 @@ class PostimagesController < ApplicationController
   end
 
   def index
+    @postimages = Postimage.all
   end
 
   def show
+    @postimage = Postimage.find(params[:id])
   end
   
+  def destroy
+    @postimage = Postimage.find(params[:id])
+    @postimage.destroy
+    redirect_to postimages_path
+  end
+
   private
-  
+
   def postimage_params
     params.require(:postimage).permit(:shop_name, :image, :caption)
-  
+
   end
 end
